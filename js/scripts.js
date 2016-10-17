d1fbfca2c2f7607ad8c61e9cbd235dd15cf2a3f9
Chris Wingo and Nigel Burnett <nigel@>
2016-10-17 16:44:16 -0700

    Add images to the UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,37 @@
-$(function() {
-  var sentence = prompt("Enter a sentence.");
 
-  alert(sentence);
+$(function() {
+  var sentence = prompt("Enter a phrase to be encoded: ");
 
   var caps = function() {
     var firstLast = sentence.slice(0, 1)+sentence.slice(-1);
     var newString = firstLast.toUpperCase();
     return newString;
-    /*var firstLetter = sentence.charAt(0);
-    var lastLetter = sentence.charAt(sentence.length - 1);
-    firstLetter = firstLetter.toUpperCase();
-    lastLetter = lastLetter.toUpperCase();
-    var newString = firstLetter + sentence.slice(1,sentence.length - 1) + lastLetter;
-    return newString;*/
   };
 
   var reverse = function(aString) {
     var reverseString = aString.split("").reverse().join("");
     reverseString = sentence + reverseString;
     return reverseString;
-
   };
 
   var charSelect = function(aString) {
     var stringLength = aString.length;
     var letter = aString.charAt(parseInt(aString.length/2));
     return letter + aString;
-  }
+  };
 
-  alert(charSelect(reverse(caps())));
-
-
-
-
-
+  var totalReversal = function(aString) {
+    var totalString = aString.split("").reverse().join("");
+    return totalString;
+  };
 
 
+  $(".image1").click(function() {
+    alert(sentence);
+  });
 
-
-
-
-
-
+  $(".image2").click(function() {
+    totalString = totalReversal(charSelect(reverse(caps())));
+    alert(totalString);
+  });
 });
