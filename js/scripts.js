5d4350700cf832cbf753edc8896c92c07b5c38b1
Chris Wingo and Nigel Burnett <nigel@>
2016-10-17 14:41:53 -0700

    Add caps to first and last letter of string

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 $(function() {
 
-var sentence = prompt("Enter a sentence.");
+  var sentence = prompt("Enter a sentence.");
 
-alert(sentence);
+  alert(sentence);
 
+  var caps = function() {
+    /*var firstLast = sentence.slice(0, 1)+sentence.slice(-1);
+    var newString = firstLast.toUpperCase();
+    return newString;*/
+    var firstLetter = sentence.charAt(0);
+    var lastLetter = sentence.charAt(sentence.length - 1);
+    firstLetter = firstLetter.toUpperCase();
+    lastLetter = lastLetter.toUpperCase();
+    var newString = firstLetter + sentence.slice(1,sentence.length - 1) + lastLetter;
+    return newString;
+  };
 
+  alert(caps());
 
 
 
